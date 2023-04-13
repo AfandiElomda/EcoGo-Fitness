8b5920ededd7a4ea415b38ead78bb4294833a324
+ <+>
2023-04-13 09:54:58 +0300

    mapped over the bodyParts data

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollBar from './HorizontalScrollBar';
 
 const SearchExercises = () => {
     const [search, setSearch] = useState('');
@@ -73,6 +74,15 @@ const SearchExercises = () => {
         >
             Search
         </Button>
+      </Box>
+      <Box
+       sx={{
+        position: 'relative',
+        width: '100%',
+        p: '20px'
+       }}
+      >
+       <HorizontalScrollBar data={bodyParts} />
       </Box>
     </Stack>
   )
