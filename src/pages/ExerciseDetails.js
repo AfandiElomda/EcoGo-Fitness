f013c92e04b6951eb99d4c7b56ed142187298fb0
+ <+>
2023-04-13 20:29:51 +0300

    implemented the similarExercises component

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -10,6 +10,8 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetails = () => {
    const [exerciseDetail, setExerciseDetail] = useState({});
    const [exerciseVideos, setExerciseVideos] = useState([]);
+   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+   const [equipmentExercises, setEquipmentExercises] = useState([]);
    const {id} = useParams();
 
    useEffect(() => {
@@ -24,6 +26,12 @@ const ExerciseDetails = () => {
        const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
        setExerciseVideos(exerciseVideosData.contents);
        
+       const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+       setTargetMuscleExercises(targetMuscleExercisesData);
+
+       const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+       setEquipmentExercises(equipmentExercisesData);
+
      }
 
      fetchExerciseData();
@@ -33,7 +41,7 @@ const ExerciseDetails = () => {
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   )
 }
