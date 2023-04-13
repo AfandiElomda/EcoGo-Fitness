a635d13a04bbdd5333a8ebc7eb8a0cceae0d936c
+ <+>
2023-04-13 15:57:27 +0300

    set the exercise videos function && bit of polishing

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetails = () => {
    const [exerciseDetail, setExerciseDetail] = useState({});
+   const [exerciseVideos, setExerciseVideos] = useState([]);
    const {id} = useParams();
 
    useEffect(() => {
@@ -19,6 +20,10 @@ const ExerciseDetails = () => {
        const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       
        setExerciseDetail(exerciseDetailData);
+
+       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
+       setExerciseVideos(exerciseVideosData.contents);
+       
      }
 
      fetchExerciseData();
@@ -26,8 +31,8 @@ const ExerciseDetails = () => {
 
   return (
     <Box>
-      <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     }
   };
 
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '318ff75a31mshd481a8e23509b6cp1b35d0jsnc21d18285fad',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
 
  const response = await fetch(url, options);
